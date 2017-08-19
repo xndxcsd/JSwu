04308c3523db37edb2d92dc6fa73cd09e421c0ea
xndxcsd <985857416@qq.com>
2017-08-19 23:27:31 +0800

    feat: update grader javadoc

--- JSwu-grade-simple/src/main/java/cn/edu/swu/SimpleGrader.java
+++ JSwu-grade-simple/src/main/java/cn/edu/swu/SimpleGrader.java
@@ -30,14 +30,47 @@ public class SimpleGrader implements Grader, ConnectionInject {
         this.swuConnection.getAccessOfJW();
     }
 
+    /**
+     * 查询已获得的所有课程成绩
+     * @return Json格式的成绩列表
+     */
     public String getJsonGrades() {
         return getJsonGrades(null, null);
     }
 
+    /**
+     * 查询某学年所有成绩，当传入null时，查询所有成绩。当传入非法学年时，等同于传入null。
+     * @param year 学年
+     * @return Json格式的成绩列表
+     */
     public String getJsonGrades(Integer year) {
         return getJsonGrades(year, null);
     }
 
+    /**
+     * 传入学年和学期查询成绩。
+     * <p>
+     *     当参数为null时，查询所有合法值。如查询2017年1、2、3学期的成绩，可作如下调用
+     * </p>
+     * <p>
+     * <code>
+     *     this.getJsonGrades(2017,null);
+     *
+     *
+     * </code>
+     * </p>
+     * 另外，如下
+     * <p>
+     * <code>
+     *     this.getJsonGrades(null,2);
+     * </code>
+     * </p>
+     * 查询所有学年的第二学期成绩的调用方式是不合法的，应选取其他方式实现。
+     * 当传入非法参数时，等同于传入null
+     * @param  year 第一个参数-学年
+     * @param  term 第二个参数-学期
+     * @return json格式的成绩列表
+     */
     public String getJsonGrades(Integer year, Integer term) {
         // TODO check parameter: throw IllegalArgumentException
         if (!swuConnection.isOpen()) {
